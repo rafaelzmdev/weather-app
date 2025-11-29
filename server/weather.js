a7d412e42497589adc464db9b37a3047763c74ed
rafaelzmdev <rafaelzmdev@gmail.com>
2025-11-29 14:58:04 -0300

    small updates
    on the way

--- server/weather.js
+++ server/weather.js
@@ -1,12 +1,11 @@
 import express from "express";
 import cors from "cors";
 const app = express()
-// Middleware to parse JSON bodies
 app.use(express.json());
 app.use(express.static('public'))
 app.use(cors());
 app.post("/api/getcity", (req, res) => {
-  const { city } = req.body;        // extract the data sent from frontend
+  const { city } = req.body;       
   if (!city) {
     return res.sendStatus(400)
   }
@@ -15,16 +14,15 @@ app.post("/api/getcity", (req, res) => {
   console.log(city)
   fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(city)}`)
   .then(response => response.json())
-  .then(data => {
+  .then (data => {
         const latitude = data.results[0].latitude;
         const longitude = data.results[0].longitude;
         console.log(latitude, longitude)
     })
-    .catch(error => console.error(error), console.log("Couldn't get proper API request. Maybe a typo in the city name?"))
-});
+    .catch(error => console.error(error));
+})
 app.listen(3000, () => console.log("Server running on port 3000"));
-//my code from here onwards
 
 
-//https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true
+https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true
 
