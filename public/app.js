366978e898a44cd0cfaea6602c375bce18cacfc2
rafaelzmdev <rafaelzmdev@gmail.com>
2025-11-29 17:28:33 -0300

    frontend now fetching weather data
    Made it so the front end now gets a JSON response from the backend, with the weather data that will be displayed in the HTML file later. Looking solid. Still got many things to do.

--- public/app.js
+++ public/app.js
@@ -1,14 +1,13 @@
-// get a button here to wait on the city name
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("inputbutton");
     const input = document.getElementById("cityname");
     button.addEventListener("click", () => {
         const city = input.value;
         fetch("http://localhost:3000/api/getcity", {
-            method: "POST",                     // sending data
+            method: "POST",                     
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({city})
-        })      // wrap it in an object
+        })      
         .then(response => { return response.json();
         })
         .then(data => console.log("Backend responded:", data))
--- server/weather.js
+++ server/weather.js
@@ -10,19 +10,35 @@ app.post("/api/getcity", (req, res) => {
     return res.sendStatus(400)
   }
   console.log("City received:", city);
-  res.json({ message: "City received"}); 
   console.log(city)
   fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(city)}`)
-  .then(response => response.json())
-  .then (data => {
-        const latitude = data.results[0].latitude;
-        const longitude = data.results[0].longitude;
-        console.log(latitude, longitude)
+   .then(response => response.json())
+   .then (data => {
+      const latitude = data.results[0].latitude;
+      const longitude = data.results[0].longitude;
+      if (!latitude || !longitude) {
+        return res.status(404).json({ message: "City not found" });
+      }
+      console.log(latitude, longitude)
+      fetch (`https://api.open-meteo.com/v1/forecast?latitude=${encodeURIComponent(latitude)}&longitude=${encodeURIComponent(longitude)}&current_weather=true`)
+        .then (response => response.json())
+        .then(weatherData => {
+          const { current_weather } = weatherData;
+          const { weathercode, is_day, windspeed, temperature } = current_weather;
+          res.json({
+            city: city,
+            weather: current_weather.weathercode,
+            daynight: current_weather.is_day,
+            wind: current_weather.windspeed,
+            temp: current_weather.temperature
+          })
+          console.log(current_weather)
+        })
     })
     .catch(error => console.error(error));
 })
 app.listen(3000, () => console.log("Server running on port 3000"));
 
 
-https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true
+
 
