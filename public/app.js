a36f4da0ad708f27496bf33efb2722547de32294
rafaelzmdev <rafaelzmdev@gmail.com>
2025-11-30 07:46:31 -0300

    Variables ready to use
    assigned the variables received from the back end to a bigger scope, so now I can finally use dom manipulation to write them on the HTML page. Time to build the real front end now!

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("inputbutton");
     const input = document.getElementById("cityname");
+    let temp, weather, daynight, wind;
     button.addEventListener("click", () => {
         const city = input.value;
         fetch("http://localhost:3000/api/getcity", {
@@ -10,7 +11,14 @@ document.addEventListener("DOMContentLoaded", () => {
         })      
         .then(response => { return response.json();
         })
-        .then(data => console.log("Backend responded:", data))
+        .then(data => {
+            weather = data.weather,
+            daynight = data.daynight,
+            wind = data.wind,
+            temp = data.temp
+            console.log(city, weather, daynight, wind, temp, "These variables should now be perfectly assigned and separated so I can use them in a bigger scope")
+        }
+    )
         .catch(err => console.error(err));
     });
 });
